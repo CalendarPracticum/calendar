fd08d3734eec6a3e58baf099ff27a2dd8c2cada1
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-04-25 08:20:12 +0300

    Update requirements.txt

--- backend/calendar_project/settings.py
+++ backend/calendar_project/settings.py
@@ -167,7 +167,7 @@ STATIC_URL = 'static/'
 # Media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media',
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
