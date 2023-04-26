13c2934b2c92d2808e577be343c26b73d0186dae
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-04-26 16:37:35 +0300

    Fix for flake8, isort

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,19 +5,27 @@ from django.contrib.auth.admin import UserAdmin
 
 @admin.register(get_user_model())
 class UserAdmin(UserAdmin):
+    """
+    Пользовательская админ-модель.
+
+    В сеты редактирования и создания экземпляра стандартной модели
+    добавлены поля аватара и количества рабочих часов.
+    """
+
     add_fieldsets = (
         (
             None,
             {
-                "classes": ("wide",),
-                "fields": (
-                    "email", "username", "profile_picture", "hours",
-                    "password1", "password2"
+                'classes': ('wide',),
+                'fields': (
+                    'email', 'username', 'profile_picture', 'hours',
+                    'password1', 'password2',
                 ),
             },
         ),
     )
     fieldsets = UserAdmin.fieldsets + (
-            (('Информация о профиле',
-              {'fields': ('profile_picture', 'hours')}),)
+        ('Информация о профиле',
+         {'fields': ('profile_picture', 'hours')},
+         ),
     )
