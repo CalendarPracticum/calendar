1eb2285b9e8975ac789db59f47afa2a23e32d78d
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-05-07 08:10:04 +0300

    Add test of events for user and admin

--- backend/events/tests/test_events_enpoints.py
+++ backend/events/tests/test_events_enpoints.py
@@ -1,14 +1,18 @@
 import os
+from unittest.mock import patch
 
+from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 
 from events.models import Event
 
+User = get_user_model()
 
-class AnonymousEventTest(APITestCase):
+
+class EventTest(APITestCase):
     """
     Тестирование доступности эндопойнтов приложения events для анонима.
     """
@@ -23,121 +27,233 @@ class AnonymousEventTest(APITestCase):
         fixture_path = os.path.join(os.path.dirname(__file__), 'data.json')
         call_command(command_name, fixture_path)
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user = User.objects.get(email='test@user.com')
+        owner = User.objects.get(email='owner@user.com')
+        admin = User.objects.get(email='ad@min.com')
+
+        cls.anon = APIClient(name='Анонимный пользователь')
+        cls.user = APIClient(name='Обычный пользователь')
+        cls.owner = APIClient(name='Владелец события')
+        cls.admin = APIClient(name='Администратор')
+
+        cls.user.force_authenticate(user)
+        cls.owner.force_authenticate(owner)
+        cls.admin.force_authenticate(admin)
+        for client in (cls.anon, cls.user, cls.owner, cls.admin):
+            client.name = client.defaults.get('name')
+
     def test_get_events_list(self):
         """
-        Тестирование доступности списка событий для анонима.
-        """
+        Получить список событий.
 
-        response = self.client.get(
-            reverse('events-list'),
-            data={
-                'datetime_start_after': '2023-01-01',
-                'datetime_start_before': '2023-12-01'
-            }
-        )
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK,
-            'Аноним должен получить статус 200 для GET-запроса на api/v1/events/ '
-        )
-
-    def test_get_one_of_global_event(self):
-        """
-        Тестирование доступности общего события для анонима.
-
-        Анонимный пользователь должен иметь доступ к событиям из глобального
+        Все пользователи должны иметь доступ к событиям из глобального
         календаря — Праздники РФ, выходные дни и прочее.
         """
 
-        response = self.client.get(reverse('events-detail', args=(1,)))
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK,
-            'Аноним должен получить статус 200 для GET-запроса на api/v1/events/<events_id>'
+        cases = (
+            (self.anon, status.HTTP_200_OK),
+            (self.user, status.HTTP_200_OK),
+            (self.admin, status.HTTP_200_OK),
         )
 
-    def test_get_one_of_users_event(self):
-        """
-        Тестирование доступности пользовательского события для анонима.
-
-        Анонимный пользователь НЕ должен иметь доступ к событиям
-        других пользователей.
-        """
-
-        event_id = Event.objects.get(name='test_event').id
-        response = self.client.get(reverse('events-detail', args=(event_id,)))
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_403_FORBIDDEN,
-            'Аноним должен получить статус 403 для GET-запроса '
-            'на api/v1/events/<events_id>, '
-            'если запрашиваемое событие принадлежит другому пользователю'
-        )
-
-    def test_delete_event(self):
-        """
-        Тестирование доступности удаления события анонимом.
-        """
-
-        response = self.client.delete(reverse('events-detail', args=(1,)))
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_403_FORBIDDEN,
-            'Аноним должен получить статус 403 для DELETE-запроса на api/v1/events/<events_id>'
-        )
-
-    def test_put_and_patch_event(self):
-        """
-        Тестирование доступности редактирования события анонимом.
-        """
-
-        name_first_event = Event.objects.get(pk=1).name
-        methods = [
-            self.client.patch,
-            self.client.put
-        ]
-        for method in methods:
-            with self.subTest(method=method.__name__):
-                response = method(
-                    reverse('events-detail', args=(1,)),
+        for client, expected_code in cases:
+            with self.subTest(client.name):
+                response = client.get(
+                    reverse('events-list'),
                     data={
-                        'datetime_start': '2023-01-01T00:00:00',
-                        'name': 'new_name',
-                        'category': 1,
-                        'calendar': 1,
+                        'datetime_start_after': '2023-01-01',
+                        'datetime_start_before': '2023-12-01'
                     }
                 )
                 self.assertEqual(
                     response.status_code,
-                    status.HTTP_403_FORBIDDEN,
-                    f'Аноним должен получить статус 403 для '
-                    f'{method.__name__.upper()}-запроса'
-                    f' на api/v1/events/<events_id>'
+                    expected_code,
+                    f'{client.name} должен получить статус {expected_code}'
+                    f' для GET-запроса на api/v1/events/ '
+                )
+
+    def test_get_one_of_global_event(self):
+        """
+        Получить общее события.
+
+        Все пользователи должны иметь доступ к событиям из глобального
+        календаря — Праздники РФ, выходные дни и прочее.
+        """
+
+        cases = (
+            (self.anon, status.HTTP_200_OK),
+            (self.user, status.HTTP_200_OK),
+            (self.admin, status.HTTP_200_OK),
+        )
+
+        for client, expected_code in cases:
+            with self.subTest(client.name):
+                response = client.get(reverse('events-detail', args=(1,)))
+                self.assertEqual(
+                    response.status_code,
+                    status.HTTP_200_OK,
+                    f'{client.name} должен получить статус {expected_code}'
+                    f' для GET-запроса на api/v1/events/<event_id>,'
+                    f'где event — общее событие'
+                )
+
+    def test_get_one_of_users_event(self):
+        """
+        Получить пользовательское событие.
+
+        Обычный пользователь и аноним НЕ должны иметь доступ к событиям
+        других пользователей.
+        """
+
+        event_id = Event.objects.get(name='test_event').id
+        cases = (
+            (self.anon, status.HTTP_403_FORBIDDEN),
+            (self.user, status.HTTP_403_FORBIDDEN),
+            (self.owner, status.HTTP_200_OK),
+            (self.admin, status.HTTP_200_OK),
+        )
+
+        for client, expected_code in cases:
+            with self.subTest(client.name):
+                response = client.get(
+                    reverse('events-detail', args=(event_id,))
                 )
                 self.assertEqual(
-                    response.data.get('name'),
-                    name_first_event,
-                    'Аноним не должен иметь доступ к редактированию событий'
+                    response.status_code,
+                    expected_code,
+                    f'{client.name} должен получить статус {expected_code} '
+                    f'для GET-запроса на api/v1/events/<event_id>'
                 )
 
-    def test_create_event(self):
+    @patch('events.models.Event.delete')
+    def test_delete_users_event(self, mock_delete):
         """
-        Тестирование доступности создания события анонимом.
+        Удаление пользовательского события.
+
+        Событие может удалить только владелец или администратор.
         """
 
-        response = self.client.post(
-            reverse('events-list'),
-            data={
-                'datetime_start': '2023-01-01T00:00:00',
-                'name': 'test_name',
-                'category': 1,
-                'calendar': 1,
-            },
-            format='json',
+        mock_delete.return_value = None
+        event_id = Event.objects.get(name='test_event').id
+        cases = (
+            (self.anon, status.HTTP_403_FORBIDDEN),
+            (self.user, status.HTTP_403_FORBIDDEN),
+            (self.owner, status.HTTP_204_NO_CONTENT),
+            (self.admin, status.HTTP_204_NO_CONTENT),
         )
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_403_FORBIDDEN,
-            'Аноним должен получить статус 403 для POST-запроса на api/v1/events/'
+
+        for client, expected_code in cases:
+            with self.subTest(client.name):
+                response = client.delete(
+                    reverse('events-detail', args=(event_id,))
+                )
+                self.assertEqual(
+                    response.status_code,
+                    expected_code,
+                    f'{client.name} должен получить статус {expected_code}'
+                    f' для DELETE-запроса на api/v1/events/<event_id>'
+                )
+
+    @patch('events.models.Event.delete')
+    def test_delete_global_event(self, mock_delete):
+        """
+        Удаление общего события.
+
+        Событие может удалить только администратор.
+        """
+
+        mock_delete.return_value = None
+        cases = (
+            (self.anon, status.HTTP_403_FORBIDDEN),
+            (self.user, status.HTTP_403_FORBIDDEN),
+            (self.owner, status.HTTP_403_FORBIDDEN),
+            (self.admin, status.HTTP_204_NO_CONTENT),
         )
+
+        for client, expected_code in cases:
+            with self.subTest(client.name):
+                response = client.delete(
+                    reverse('events-detail', args=(1,))
+                )
+                self.assertEqual(
+                    response.status_code,
+                    expected_code,
+                    f'{client.name} должен получить статус {expected_code}'
+                    f' для DELETE-запроса на api/v1/events/<event_id>,'
+                    f'где event — общее событие'
+                )
+
+    def test_put_and_patch_global_event(self):
+        """
+        Редактирования общего события.
+
+        Общее событие может редактировать только администратор.
+        Поле calendar должно валидироваться: при создании пользователь может
+        указать только тот календарь, владельцем которого он является.
+        """
+
+        cases = (
+            (self.anon, status.HTTP_403_FORBIDDEN),
+            (self.user, status.HTTP_403_FORBIDDEN),
+            (self.owner, status.HTTP_400_BAD_REQUEST),
+            (self.admin, status.HTTP_200_OK),
+        )
+
+        for client, expected_code in cases:
+            for method in [client.put, client.patch]:
+                with self.subTest((client.name, method.__name__)):
+
+                    response = method(
+                        reverse('events-detail', args=(1,)),
+                        data={
+                            'datetime_start': '2023-01-01T00:00:00',
+                            'name': 'new_name',
+                            'category': 1,
+                            'calendar': 1,
+                        }
+                    )
+                    self.assertEqual(
+                        response.status_code,
+                        expected_code,
+                        f'{client.name} должен получить статус {expected_code} '
+                        f'для {method.__name__.upper()}-запроса '
+                        f'на api/v1/events/<events_id>,'
+                        f'где event — общее событие'
+                    )
+
+    def test_create_event_with_admin_calendar(self):
+        """
+        Создание общего события в глобальном календаре.
+
+        Анонимные пользователи не могут создавать события.
+        Поле calendar должно валидироваться: при создании пользователь может
+        указать только тот календарь, владельцем которого он является.
+        """
+
+        cases = (
+            (self.anon, status.HTTP_403_FORBIDDEN),
+            (self.user, status.HTTP_400_BAD_REQUEST),
+            (self.admin, status.HTTP_201_CREATED),
+        )
+
+        for client, expected_code in cases:
+            with self.subTest(client.name):
+                response = client.post(
+                    reverse('events-list'),
+                    data={
+                        'datetime_start': '2023-01-01T00:00:00',
+                        'name': 'test_name',
+                        'category': 1,
+                        'calendar': 1,
+                    },
+                    format='json',
+                )
+                self.assertEqual(
+                    response.status_code,
+                    expected_code,
+                    f'{client.name} должен получить статус {expected_code}'
+                    f' для POST-запроса на api/v1/events/'
+                )
