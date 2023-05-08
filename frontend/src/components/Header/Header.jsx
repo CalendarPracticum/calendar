406501664ca4a25ba54b68770b716c9ff0233ca4
Olga Latkina <olga.latkina@gmail.com>
2023-05-08 20:29:03 +0700

    refactor: add temporary logo handler

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,36 +7,35 @@ import styles from './Header.module.css';
 import CurrentUserContext from '../../context/CurrentUserContext';
 
 export function Header({ loggedIn }) {
-	const userContext = useContext(CurrentUserContext);
-	const { currentUser, setLoggedIn } = userContext;
-	const { name, email } = currentUser;
+  const userContext = useContext(CurrentUserContext);
+  const { currentUser, setLoggedIn } = userContext;
+  const { name, email } = currentUser;
 
-	const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
-	return (
-		<header className={styles.header}>
-			<div className={`${styles.wrapper} container`}>
-				<div>{loggedIn ? `${name} ${email}` : 'Logo'}</div>
-				<div className={styles.switchGroup}>
-					<i className="pi pi-moon" style={{ fontSize: '1.5rem' }} />
-					<InputSwitch
-						checked={checked}
-						onChange={(e) => setChecked(e.value)}
-					/>
-					<i className="pi pi-sun" style={{ fontSize: '1.5rem' }} />
-				</div>
-				<Button
-					label="Войти"
-					size="small"
-					outlined
-					className={styles.button}
-					onClick={() => setLoggedIn(true)}
-				/>
-			</div>
-		</header>
-	);
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.wrapper} container`}>
+        <button type='button' onClick={() => setLoggedIn(true)}>{loggedIn ? `${name} ${email}` : 'Logo'}</button>
+        <div className={styles.switchGroup}>
+          <i className="pi pi-moon" style={{ fontSize: '1.5rem' }} />
+          <InputSwitch
+            checked={checked}
+            onChange={(e) => setChecked(e.value)}
+          />
+          <i className="pi pi-sun" style={{ fontSize: '1.5rem' }} />
+        </div>
+        <Button
+          label="Войти"
+          size="small"
+          outlined
+          className={styles.button}
+        />
+      </div>
+    </header>
+  );
 }
 
 Header.propTypes = {
-	loggedIn: PropTypes.bool.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
 };
