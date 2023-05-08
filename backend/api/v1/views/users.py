302b462d314e4356a9c84a448055446b7ad45c6d
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-05-08 18:17:31 +0300

    Fix flake8. Remove unused import

--- backend/api/v1/views/users.py
+++ backend/api/v1/views/users.py
@@ -1,12 +1,10 @@
 from django.contrib.auth import get_user_model
-from drf_yasg import openapi
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets, mixins, status
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from api.v1.serializers.users import UsersSerializer, UsersCreateSerializer
+from api.v1.serializers.users import UsersCreateSerializer, UsersSerializer
 
 User = get_user_model()
 
@@ -25,7 +23,7 @@ class UsersViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (AllowAny,)
 
     @action(
-        ["get", "patch", "delete"],
+        ['get', 'patch', 'delete'],
         detail=False,
         permission_classes=[IsAuthenticated],
         serializer_class=UsersSerializer,
@@ -37,30 +35,33 @@ class UsersViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
         Доступны GET, PATCH и DELETE запросы.
         Для DELETE-запроса требуется current_password.
         """
-        if request.method == 'GET':
-            serializer = self.serializer_class(request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
 
-        elif request.method == 'PATCH':
-            serializer = self.serializer_class(
-                request.user, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
+        match request.method:
+            case 'GET':
+                serializer = self.serializer_class(request.user)
                 return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
 
-        elif request.method == 'DELETE':
-            password = request.data.get('current_password')
-            if not password:
-                return Response(
-                    data={'current_password': 'Требуется пароль'},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            if not request.user.check_password(password):
-                return Response(
-                    data={'current_password': 'Неверный пароль'},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            request.user.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            case 'PATCH':
+                serializer = self.serializer_class(
+                    request.user, data=request.data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            case 'DELETE':
+                password = request.data.get('current_password')
+                if not password:
+                    return Response(
+                        data={'current_password': 'Требуется пароль'},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                if not request.user.check_password(password):
+                    return Response(
+                        data={'current_password': 'Неверный пароль'},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                request.user.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response()
