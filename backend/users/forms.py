a144b4ee80536d1cb8a40d566b35cd9d315fb705
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-05-11 14:32:35 +0300

    Remove blank line in imports of forms

--- backend/users/forms.py
+++ backend/users/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import gettext_lazy as _
 
-
 User = get_user_model()
 
 
