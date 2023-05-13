e3fd96e054a90188a52cd2e1e7847e341bac7b8b
Dmitry Rotanin <rotanin@gmail.com>
2023-05-13 17:58:50 +0600

    del comment for debug

--- backend/calendar_project/settings.py
+++ backend/calendar_project/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# FIXME При деплое исправить на False
 DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
