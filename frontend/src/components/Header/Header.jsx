f6fe26719cc76ec32ec7a6b12bb669c705354f53
e-zybkin <zybkin.evgeniy@gmail.com>
2023-05-13 16:35:14 +0300

    feat: switch themes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import 'primeicons/primeicons.css';
 import { Button } from 'primereact/button';
@@ -12,10 +12,26 @@ export function Header({ onLogin }) {
 	const { name, email } = currentUser;
 
 	const [value, setValue] = useState('light');
+	const [darkMode, setDarkMode] = useState(false);
 	const themeOptions = [
-		{ icon: 'pi pi-moon', value: 'dark' },
-		{ icon: 'pi pi-sun', value: 'light' },
+		{ icon: 'pi pi-moon', value: 'dark', constant: darkMode },
+		{ icon: 'pi pi-sun', value: 'light', constant: !darkMode },
 	];
+
+	useEffect(() => {
+		const themeLink = document.getElementById('app-theme');
+
+		const darkTheme = 'lara-dark-indigo';
+
+		const lightTheme = 'lara-light-indigo';
+
+		if (themeLink) {
+			themeLink.href = `${process.env.PUBLIC_URL}/themes/${
+				darkMode ? darkTheme : lightTheme
+			}/theme.css`;
+		}
+	}, [darkMode]);
+
 	const themeTemplate = (option) => <i className={option.icon} />;
 
 	// TODO: добавить логотип, удалить логику показа логотипа или данных из контекста
@@ -27,9 +43,13 @@ export function Header({ onLogin }) {
 					<SelectButton
 						value={value}
 						options={themeOptions}
-						onChange={(e) => setValue(e.value)}
+						onChange={(e) => {
+							setValue(e.value);
+							setDarkMode(!darkMode);
+						}}
 						itemTemplate={themeTemplate}
 						optionLabel="value"
+						optionDisabled="constant"
 					/>
 				</div>
 				<Button
