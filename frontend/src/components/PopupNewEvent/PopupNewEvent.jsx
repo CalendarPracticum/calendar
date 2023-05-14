10ae5abaf869ac38e7d1edf44159efcd6501beb5
Olga Latkina <olga.latkina@gmail.com>
2023-05-15 05:44:52 +0700

    feat: add Dropdown with current color

--- frontend/src/components/PopupNewEvent/PopupNewEvent.jsx
+++ frontend/src/components/PopupNewEvent/PopupNewEvent.jsx
@@ -1,161 +1,212 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useForm, Controller } from 'react-hook-form';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Checkbox } from 'primereact/checkbox';
+import { Dropdown } from 'primereact/dropdown';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { classNames as cn } from 'primereact/utils';
 import styles from './PopupNewEvent.module.css';
 
 export function PopupNewEvent({ visible, setVisible }) {
-  const defaultValues = {
-    name: '',
-    timeStart: '',
-    timeFinish: '',
-    allDay: false,
-    calendar: null,
-    description: '',
-  };
+	const circle = useRef(null);
+	const calendars = [{ name: 'Личный', color: '#91DED3' }];
 
-  const {
-    control,
-    formState: { errors, isValid },
-    handleSubmit,
-    reset,
-  } = useForm({ defaultValues, mode: 'onBlur' });
+	const defaultValues = {
+		name: '',
+		timeStart: '',
+		timeFinish: '',
+		allDay: false,
+		calendar: null,
+		description: '',
+	};
 
-  const onSubmit = (data) => {
-    console.log({ data });
-    setVisible(false);
+	const {
+		control,
+		formState: { errors, isValid },
+		handleSubmit,
+		reset,
+		getValues,
+	} = useForm({ defaultValues, mode: 'onBlur' });
 
-    reset();
-  };
+	const onSubmit = (data) => {
+		console.log({ data });
+		setVisible(false);
 
-  const getFormErrorMessage = (name) =>
-    errors[name] && <small className="p-error">{errors[name].message}</small>;
+		reset();
+	};
 
-  const handleOverlayClick = (evt) => {
-    if (evt.target === evt.currentTarget) {
-      setVisible(false);
-    }
-  };
+	const onDropdownChange = () => {
+		const values = getValues();
+		const color = values?.calendar?.color;
 
-  return (
-    <Dialog
-      visible={visible}
-      onHide={() => setVisible(false)}
-      onMaskClick={handleOverlayClick}
-      blockScroll
-    >
-      <div className={styles.paddings}>
-        <div className="flex justify-content-center">
-          <div className={styles.card}>
-            <h2 className="text-center">Создайте новое событие</h2>
+		circle.current.style.color = color || 'var(--primary-color)';
+	};
 
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className={`p-fluid ${styles.form}`}
-            >
-              <div className={styles.field}>
-                <span className="p-float-label p-input-icon-right">
-                  <i className="pi pi-calendar-plus" />
-                  <Controller
-                    name="name"
-                    control={control}
-                    rules={{
-                      minLength: 1,
-                      maxLength: {
-                        value: 100,
-                        message: 'Максимальная длина 100 символа.',
-                      },
-                    }}
-                    render={({ field, fieldState }) => (
-                      <InputText
-                        id={field.name}
-                        {...field}
-                        autoFocus
-                        className={cn({
-                          'p-invalid': fieldState.invalid,
-                        })}
-                      />
-                    )}
-                  />
-                  {/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
-                  <label
-                    htmlFor="name"
-                    className={cn({ 'p-error': errors.name })}
-                  >
-                    Название
-                  </label>
-                </span>
-                {getFormErrorMessage('name')}
-              </div>
+	const getFormErrorMessage = (name) =>
+		errors[name] && <small className="p-error">{errors[name].message}</small>;
 
-              <div className={styles.field}>
-                <span className="flex align-items-center gap-2">
-                  <Controller
-                    name="allDay"
-                    control={control}
-                    render={({ field }) => (
-                      <Checkbox
-                        id={field.name}
-                        onChange={(e) => field.onChange(e.checked)}
-                        checked={field.value}
-                        inputRef={field.ref}
-                        {...field}
-                      />
-                    )}
-                  />
-                  <label
-                    htmlFor="allDay"
-                    className={cn({ 'p-error': errors.allDay })}
-                  >
-                    Весь день
-                  </label>
-                </span>
-              </div>
+	const handleOverlayClick = (evt) => {
+		if (evt.target === evt.currentTarget) {
+			setVisible(false);
+		}
+	};
 
-              <div className={styles.field}>
-                <Controller
-                  name="description"
-                  control={control}
-                  render={({ field }) => (
-                    <span className="p-float-label">
-                      <InputTextarea
-                        id={field.name}
-                        {...field}
-                        rows={4}
-                        cols={30}
-                        autoResize
-                      />
-                      <label
-                        htmlFor={field.name}
-                        className={cn({ 'p-error': errors.description })}
-                      >
-                        Описание
-                      </label>
-                    </span>
-                  )}
-                />
-              </div>
+	return (
+		<Dialog
+			visible={visible}
+			onHide={() => setVisible(false)}
+			onMaskClick={handleOverlayClick}
+			blockScroll
+		>
+			<div className={styles.paddings}>
+				<div className="flex justify-content-center">
+					<div className={styles.card}>
+						<h2 className="text-center">Создайте новое событие</h2>
 
-              <Button
-                type="submit"
-                label="Добавить новое событие"
-                className="mt-2"
-                disabled={!isValid}
-              />
-            </form>
-          </div>
-        </div>
-      </div>
-    </Dialog>
-  );
+						<form
+							onSubmit={handleSubmit(onSubmit)}
+							className={`p-fluid ${styles.form}`}
+						>
+							<div className={styles.field}>
+								<span className="p-float-label p-input-icon-right">
+									<i className="pi pi-calendar-plus" />
+									<Controller
+										name="name"
+										control={control}
+										rules={{
+											minLength: 1,
+											maxLength: {
+												value: 100,
+												message: 'Максимальная длина 100 символа.',
+											},
+										}}
+										render={({ field, fieldState }) => (
+											<InputText
+												id={field.name}
+												{...field}
+												autoFocus
+												className={cn({
+													'p-invalid': fieldState.invalid,
+												})}
+											/>
+										)}
+									/>
+									{/* eslint jsx-a11y/label-has-associated-control: ["error", { assert: "either" } ] */}
+									<label
+										htmlFor="name"
+										className={cn({ 'p-error': errors.name })}
+									>
+										Название
+									</label>
+								</span>
+								{getFormErrorMessage('name')}
+							</div>
+
+							<div className={styles.field}>
+								<span className="flex align-items-center gap-2">
+									<Controller
+										name="allDay"
+										control={control}
+										render={({ field }) => (
+											<Checkbox
+												id={field.name}
+												onChange={(e) => field.onChange(e.checked)}
+												checked={field.value}
+												inputRef={field.ref}
+												{...field}
+											/>
+										)}
+									/>
+									<label
+										htmlFor="allDay"
+										className={cn({ 'p-error': errors.allDay })}
+									>
+										Весь день
+									</label>
+								</span>
+							</div>
+
+							<div className={styles.field}>
+								<span className="flex align-items-center gap-3">
+									<i
+										ref={circle}
+										className="pi pi-circle-fill"
+										style={{
+											fontSize: '2.75em',
+											color: 'var(--primary-color)',
+										}}
+									/>
+									<Controller
+										name="calendar"
+										control={control}
+										rules={{ required: 'Календарь - поле обязательное.' }}
+										render={({ field, fieldState }) => (
+											<Dropdown
+												id={field.name}
+												value={field.value}
+												optionLabel="name"
+												placeholder="Выберите календарь"
+												options={calendars}
+												showClear
+												focusInputRef={field.ref}
+												onChange={(e) => {
+													field.onChange(e.value);
+													onDropdownChange();
+												}}
+												className={cn(
+													{ 'p-invalid': fieldState.error },
+													'w-full md:w-24rem'
+												)}
+												styles={{ width: '100%' }}
+											/>
+										)}
+									/>
+								</span>
+								{getFormErrorMessage('calendar')}
+							</div>
+
+							<div className={styles.field}>
+								<Controller
+									name="description"
+									control={control}
+									render={({ field }) => (
+										<span className="p-float-label">
+											<InputTextarea
+												id={field.name}
+												{...field}
+												rows={4}
+												cols={30}
+												autoResize
+											/>
+											<label
+												htmlFor={field.name}
+												className={cn({ 'p-error': errors.description })}
+											>
+												Описание
+											</label>
+										</span>
+									)}
+								/>
+							</div>
+
+							<Button
+								type="submit"
+								label="Добавить новое событие"
+								className="mt-2"
+								disabled={!isValid}
+							/>
+						</form>
+					</div>
+				</div>
+			</div>
+		</Dialog>
+	);
 }
 
 PopupNewEvent.propTypes = {
-  visible: PropTypes.bool.isRequired,
-  setVisible: PropTypes.func.isRequired,
+	visible: PropTypes.bool.isRequired,
+	setVisible: PropTypes.func.isRequired,
 };
