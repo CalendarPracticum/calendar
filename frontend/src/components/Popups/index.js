9440b44fc6338f6c89074e79c427f4961fd20510
Olga Latkina <olga.latkina@gmail.com>
2023-06-12 10:13:59 +0700

    refactor: common directory for popups

---
+++ frontend/src/components/Popups/index.js
@@ -0,0 +1,15 @@
+import { PopupChangePassword } from './PopupChangePassword';
+import { PopupEditCalendar } from './PopupEditCalendar';
+import { PopupEditUser } from './PopupEditUser';
+import { PopupLogin } from './PopupLogin';
+import { PopupNewCalendar } from './PopupNewCalendar';
+import { PopupNewEvent } from './PopupNewEvent';
+
+export {
+	PopupChangePassword,
+	PopupEditCalendar,
+	PopupEditUser,
+	PopupLogin,
+	PopupNewCalendar,
+	PopupNewEvent,
+};
