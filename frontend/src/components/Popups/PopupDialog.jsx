63f13e363fe7108f366082127a7272534529b973
Olga Latkina <olga.latkina@gmail.com>
2023-06-15 13:36:29 +0700

    chore: formating

--- frontend/src/components/Popups/PopupDialog.jsx
+++ frontend/src/components/Popups/PopupDialog.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 
 export function PopupDialog({
-  showMessage,
-  setShowMessage,
-  isDialogError,
-  dialogMessage,
+	showMessage,
+	setShowMessage,
+	isDialogError,
+	dialogMessage,
 }) {
-  const dialogFooter = (
+	const dialogFooter = (
 		<div className="flex justify-content-center">
 			<Button
 				label="OK"
@@ -20,34 +20,34 @@ export function PopupDialog({
 		</div>
 	);
 
-  return(
-    <Dialog
-      visible={showMessage}
-      onHide={() => setShowMessage(false)}
-      position="top"
-      footer={isDialogError ? dialogFooter : ''}
-      showHeader={false}
-      breakpoints={{ '960px': '80vw' }}
-      style={{ width: '30vw' }}
-    >
-      <div className="flex justify-content-center flex-column pt-6 px-3">
-        <i
-          className={`pi pi-${isDialogError ? 'times' : 'check'}-circle`}
-          style={{
-            fontSize: '5rem',
-            color: `var(--${isDialogError ? 'red' : 'green'}-500)`,
-          }}
-        />
-        <h4>{isDialogError ? 'Произошла ошибка!' : 'Успех!'}</h4>
-        <p style={{ lineHeight: 1.5 }}>{dialogMessage}</p>
-      </div>
-    </Dialog>
-  )
+	return (
+		<Dialog
+			visible={showMessage}
+			onHide={() => setShowMessage(false)}
+			position="top"
+			footer={isDialogError ? dialogFooter : ''}
+			showHeader={false}
+			breakpoints={{ '960px': '80vw' }}
+			style={{ width: '30vw' }}
+		>
+			<div className="flex justify-content-center flex-column pt-6 px-3">
+				<i
+					className={`pi pi-${isDialogError ? 'times' : 'check'}-circle`}
+					style={{
+						fontSize: '5rem',
+						color: `var(--${isDialogError ? 'red' : 'green'}-500)`,
+					}}
+				/>
+				<h4>{isDialogError ? 'Произошла ошибка!' : 'Успех!'}</h4>
+				<p style={{ lineHeight: 1.5 }}>{dialogMessage}</p>
+			</div>
+		</Dialog>
+	);
 }
 
 PopupDialog.propTypes = {
-  showMessage: PropTypes.bool.isRequired,
-  setShowMessage: PropTypes.func.isRequired,
-  isDialogError: PropTypes.bool.isRequired,
-  dialogMessage: PropTypes.string.isRequired,
+	showMessage: PropTypes.bool.isRequired,
+	setShowMessage: PropTypes.func.isRequired,
+	isDialogError: PropTypes.bool.isRequired,
+	dialogMessage: PropTypes.string.isRequired,
 };
