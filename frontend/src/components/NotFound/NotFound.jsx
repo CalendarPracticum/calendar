191fc7df442f60fe6473f5469a733abbbfab6db4
Olga Latkina <olga.latkina@gmail.com>
2023-06-23 01:38:55 +0700

    feat: 404 animation

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,49 +1,73 @@
-/* eslint-disable react/prop-types */
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './NotFound.module.css';
 
-const ItemLeft = ({ text, icon }) => (
-	<div className={styles.itemLeft}>{`${text} ${icon}`}</div>
-);
+// const ItemLeft = ({ text, icon }) => (
+//   <div className={styles.itemLeft} >{`${text} ${icon}`}</div>
+// );
 
-const ItemRight = ({ text, icon }) => (
-	<div className={styles.itemRight}>{`${text} ${icon}`}</div>
-);
+// const ItemRight = ({ text, icon }) => (
+//   <div className={styles.itemRight} >{`${text} ${icon}`}</div>
+// );
 
-const dialogData = [
-	{
-		text: 'Ошибка 404, страница не найдена',
-		icon: '😔',
-		isLeft: true,
-	},
-	{
-		text: 'И что же мне делать?',
-		icon: '😮',
-		isLeft: false,
-	},
-	{
-		text: 'Возвращайся на Главную!',
-		icon: '💃',
-		isLeft: true,
-	},
-	{
-		text: 'А как это сделать?',
-		icon: '🤔',
-		isLeft: false,
-	},
-	{
-		text: 'Нажать на большую жёлтую кнопку',
-		icon: '🚀',
-		isLeft: true,
-	},
-	{
-		text: 'Так бы сразу и сказали!',
-		icon: '😅',
-		isLeft: false,
-	},
-];
+// const dialogData = [
+//   {
+//     id: 'one',
+//     text: 'Ошибка 404, страница не найдена',
+//     icon: '😔',
+//     isLeft: true,
+//   },
+//   {
+//     id: 'two',
+//     text: 'И что же мне делать?',
+//     icon: '😮',
+//     isLeft: false,
+//   },
+//   {
+//     id: 'three',
+//     text: 'Возвращайся на Главную!',
+//     icon: '💃',
+//     isLeft: true,
+//   },
+//   {
+//     id: 'four',
+//     text: 'А как это сделать?',
+//     icon: '🤔',
+//     isLeft: false,
+//   },
+//   {
+//     id: 'five',
+//     text: 'Нажать на большую жёлтую кнопку',
+//     icon: '🚀',
+//     isLeft: true,
+//   },
+//   {
+//     id: 'six',
+//     text: 'Так бы сразу и сказали!',
+//     icon: '😅',
+//     isLeft: false,
+//   },
+// ];
 
 export function NotFound() {
+	const nums = useRef(null);
+	const one = useRef(null);
+	const two = useRef(null);
+	const three = useRef(null);
+	const four = useRef(null);
+	const five = useRef(null);
+	const six = useRef(null);
+
+	useEffect(() => {
+		nums.current.classList.add(`${styles.onShowNums}`);
+		one.current.classList.add(`${styles.onShowOne}`);
+		two.current.classList.add(`${styles.onShowTwo}`);
+		three.current.classList.add(`${styles.onShowThree}`);
+		four.current.classList.add(`${styles.onShowFour}`);
+		five.current.classList.add(`${styles.onShowFive}`);
+		six.current.classList.add(`${styles.onShowSix}`);
+	}, []);
+
 	return (
 		<div className={styles.notFound}>
 			<div className={styles.wrapper}>
@@ -59,19 +83,39 @@ export function NotFound() {
 					<div className={styles.line} />
 				</div>
 				<div className={styles.dialog}>
-					{dialogData.map(({ text, icon, isLeft }) =>
-						isLeft ? (
-							<ItemLeft text={text} icon={icon} key={text} />
-						) : (
-							<ItemRight text={text} icon={icon} key={text} />
-						)
-					)}
+					{/* {dialogData.map(({ text, icon, isLeft, id }) =>
+            isLeft ? (
+              <ItemLeft text={text} icon={icon} key={id} />
+            ) : (
+              <ItemRight text={text} icon={icon} key={id} />
+            )
+          )} */}
+					<div className={styles.itemLeft} ref={one} key="one">
+						`Ошибка 404, страница не найдена 😔`
+					</div>
+					<div className={styles.itemRight} ref={two} key="two">
+						`И что же мне делать? 😮`
+					</div>
+					<div className={styles.itemLeft} ref={three} key="three">
+						`Возвращайся на Главную! 💃`
+					</div>
+					<div className={styles.itemRight} ref={four} key="four">
+						`А как это сделать? 🤔`
+					</div>
+					<div className={styles.itemLeft} ref={five} key="five">
+						`Нажать на большую жёлтую кнопку 🚀`
+					</div>
+					<div className={styles.itemRight} ref={six} key="six">
+						`Так бы сразу и сказали! 😅`
+					</div>
 				</div>
-				<div className={styles.nums}>
+
+				<div className={styles.nums} ref={nums}>
 					<div className={styles.numFourStart} />
 					<div className={styles.numZero} />
 					<div className={styles.numFourEnd} />
 				</div>
+
 				<Link to="/" className={styles.link}>
 					Большая жёлтая кнопка
 				</Link>
