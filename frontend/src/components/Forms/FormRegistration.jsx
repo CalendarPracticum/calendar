6fb6dd0b196737fc2ad7f42be36545c2ddd3f732
Olga Latkina <olga.latkina@gmail.com>
2023-06-28 21:21:45 +0700

    fix: regexp password

--- frontend/src/components/Forms/FormRegistration.jsx
+++ frontend/src/components/Forms/FormRegistration.jsx
@@ -107,7 +107,8 @@ export function FormRegistration({ showFormLogin, handleRegister }) {
 											message: 'Максимальная длина 40 символа.',
 										},
 										pattern: {
-											value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,42})/,
+											value:
+												/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,40}/,
 											message: 'Не корректный пароль',
 										},
 									}}
